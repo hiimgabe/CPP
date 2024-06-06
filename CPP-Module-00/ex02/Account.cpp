69f095b9720611d25586dea9257f0590fe94a803
hiimgabe <gabrielgmg2009@gmail.com>
2024-06-06 16:47:07 +0100

    cpp00-02 organizing

---
+++ CPP-Module-00/ex02/Account.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabe <gabe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 16:32:45 by gabe              #+#    #+#             */
+/*   Updated: 2024/06/06 16:42:29 by gabe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) {}
+
+Account::~Account(void) {}
+
+int Account::getNbAccounts(void)
+{
+}
+
+int Account::getTotalAmount(void) {}
+
+int Account::getNbDeposits(void) {}
+
+int	Account::getNbWithdrawals(void) {}
+
+void	Account::displayAccountsInfos(void) {}
+
+void	Account::makeDeposit(int deposit) {}
+
+bool	Account::makeWithdrawal(int withdrawal) {}
+
+int	Account::checkAmount(void) const{}
+
+void	Account::displayStatus(void) const{}
