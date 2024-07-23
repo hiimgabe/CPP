67a2612130a85f9268a8527b2ffc4350c3f04c41
hiimgabe <gabrielgmg2009@gmail.com>
2024-07-23 18:45:40 +0100

    CPP01 ex04 done(i think).
    Yes that lovely poem was written by me to test the program. No I am not proud.

---
+++ CPP-Module-01/ex04/main.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabe <gabe@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 15:25:35 by gabe              #+#    #+#             */
+/*   Updated: 2024/07/23 18:44:30 by gabe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+void	sandr(std::string filename, std::string original, std::string search, std::string replace)
+{
+	int			i = 0;
+	int			search_length = search.length();
+	std::string	res = "";
+	std::ofstream	sandr_file;
+	std::string		replace_name = filename + ".replace";
+	
+	if (search.empty())
+	{
+		sandr_file.open(replace_name.c_str());
+		if (sandr_file.fail())
+			throw std::runtime_error("Failed to create the outfile.");
+		sandr_file << original;
+		sandr_file.close();
+	}
+	else
+	{
+		while (i < original.length())
+		{
+			if (original.substr(i, search_length) == search)
+			{
+				res += replace;
+				i += search_length;
+			}
+			else
+			{
+				res += original[i];
+				i++;
+			}
+		}
+		sandr_file.open(replace_name.c_str());
+		if (sandr_file.fail())
+			throw std::runtime_error("Failed to create the outfile.");
+		sandr_file << res;
+		sandr_file.close();
+	}
+}
+
+std::string	read_content(std::string str)
+{
+	std::ifstream	file;
+	std::string		buffer;
+	std::string		content;
+
+	file.open(str);
+	if (file.fail())
+		throw std::runtime_error("Couldn't open the file.");
+	while (std::getline(file, buffer))
+		content.append(buffer + '\n');
+	file.close();
+	return (content);
+}
+
+void	input_instructions()
+{
+	std::cout << "./sandr <filename> <s1> <s2>" << std::endl;
+	throw std::invalid_argument("Invalid arguments.");
+}
+
+int	main(int argc, char **argv)
+{
+	try
+	{
+		if (argc != 4)
+			input_instructions();
+		std::string file = read_content(argv[1]);
+		std::string search = argv[2];
+		std::string replace = argv[3];
+		sandr(argv[1], file, search, replace);
+		std::cout << ".replace file created successfully" << std::endl;
+		return (EXIT_SUCCESS);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+		return (EXIT_FAILURE);
+	}
+	
+}
