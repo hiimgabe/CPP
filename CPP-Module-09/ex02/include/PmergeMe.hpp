e5ac5110b3c1f068b67427dd41eda15d8f14edd8
hiimgabe <gabrielgmg2009@gmail.com>
2025-02-24 16:15:12 +0000

    PmergeMe start

---
+++ CPP-Module-09/ex02/include/PmergeMe.hpp
@@ -0,0 +1,17 @@
+
+#ifndef	PMERGEME_HPP
+#define	PMERGEME_HPP
+
+class PmergeMe
+{
+	public:
+		PmergeMe(void);
+		PmergeMe(const PmergeMe &rhs);
+		PmergeMe &operator=(const PmergeMe &rhs);
+		~PmergeMe(void);
+	
+	private:
+		
+};
+
+#endif 
