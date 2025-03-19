03b15e658bd99ec0e9cc0217d7df5b6df42bbd65
Gabriel Moreira Gomes <gamoreir@c2r4s6.42porto.com>
2025-03-19 10:10:29 +0000

    added deque and list tests to ex00

--- CPP-Module-08/ex00/include/EasyFind.hpp
+++ CPP-Module-08/ex00/include/EasyFind.hpp
@@ -10,7 +10,7 @@ int	find(T data, int value)
 {
 	typename T::iterator	res = std::find(data.begin(), data.end(), value);
 	if (res != data.end())
-		return (std::distance(data.begin(), res));
+		return (*res);
 	throw std::runtime_error("No such value.");
 }
 
--- CPP-Module-08/ex00/source/main.cpp
+++ CPP-Module-08/ex00/source/main.cpp
@@ -1,13 +1,17 @@
 #include "../include/EasyFind.hpp"
 #include <vector>
+#include <list>
+#include <deque>
 
-void	intTest()
+void	intVectorTest()
 {
 	std::vector<int>	intVector;
+	
+	std::cout << "\nworking with int vector:\n\n";
 	for (int i = 0; i < 6; i++)
 	{
 		intVector.push_back(i * 2);
-		std::cout << "intVector[" << i << "] : " << intVector[i] << std::endl;
+		std::cout << "intVector[" << i << "] = " << intVector[i] << std::endl;
 	}
 
 	std::cout << "\n==== Testing ===\n" << std::endl;
@@ -22,9 +26,57 @@ void	intTest()
 	}
 }
 
+void	intDequeTest()
+{
+	std::deque<int>	intDeque;
+	
+	std::cout << "\n\nworking with int deque:\n\n";
+	for (int i = 0; i < 6; i++)
+	{
+		intDeque.push_back(i * 2);
+		std::cout << "intDeque[" << i << "] = " << intDeque[i] << std::endl;
+	}
+
+	std::cout << "\n==== Testing ===\n" << std::endl;
+	try
+	{
+		std::cout << "Checking for: 10\t" << find(intDeque, 10) << std::endl;
+		std::cout << "Checking for: 3\t\t" << find(intDeque, 3) << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void	intListTest()
+{
+	std::list<int>	intList;
+	
+	std::cout << "\n\nworking with int list:\n\n";
+	for (int i = 0; i < 6; i++)
+	{
+		intList.push_back(i * 2);
+		std::cout << "intList i: " << i << " = " << i * 2 << std::endl;
+	}
+
+	std::cout << "\n==== Testing ===\n" << std::endl;
+	try
+	{
+		std::cout << "Checking for: 10\t" << find(intList, 10) << std::endl;
+		std::cout << "Checking for: 3\t\t" << find(intList, 3) << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
 int	main(void)
 {
-	intTest();
+	intVectorTest();
+	intListTest();
+	intDequeTest();
 	
 	return (0);
 }
